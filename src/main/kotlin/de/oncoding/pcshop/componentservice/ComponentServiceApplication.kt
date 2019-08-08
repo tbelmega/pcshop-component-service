209b2d88beaa92209d717e97f38ee0d2d344dc49
Thiemo Belmega <thiemo.belmega@gmail.com>
2019-08-08 22:06:00 +0200

    Generate project with Spring Initializr

---
+++ src/main/kotlin/de/oncoding/pcshop/componentservice/ComponentServiceApplication.kt
@@ -0,0 +1,11 @@
+package de.oncoding.pcshop.componentservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ComponentServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ComponentServiceApplication>(*args)
+}
---
+++ src/test/kotlin/de/oncoding/pcshop/componentservice/ComponentServiceApplicationTests.kt
@@ -0,0 +1,16 @@
+package de.oncoding.pcshop.componentservice
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ComponentServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
